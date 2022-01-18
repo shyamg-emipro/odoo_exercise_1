c9e4466dbbd48d2e4e75f9ce08b1ff5b2511db3c
shyamg <shyamg@emiprotechnologies.com>
2022-01-18 17:37:37 +0530

    [Add] estate/__manifest__.py

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'estate',
+    'summary': 'My Custom Module',
+    'description': "",
+    'depends': [
+        'base_setup'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
